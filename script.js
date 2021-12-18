cca31140f683ea95e5dabda1c9352fa9a2b87dd6
marevich <maksimrozette@gmail.com>
2021-12-18 18:46:08 +0300

    fixed problem and visual adaptive

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ buttonShow.addEventListener('click', showBlock);
 function showBlock() {
 	block.style = "display: flex";
 }
+
+// Inner button TEXT all elements !
+let buttonText = document.querySelectorAll('.item--button');
+for(let p = 0; p < buttonText.length; p++) {
+	buttonText[p].innerHTML = 'Add to card';
+}
+
+var num = 70; //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.header').addClass('fixed');
+    } else {
+        $('.header').removeClass('fixed');
+    }
+});
